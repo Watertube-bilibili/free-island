48d3afee1703825d651a8c5c211be7dc72d73017
watertube-bilibili <outlook648100@gmail.com>
2026-09-16 08:55:29 +0800

    Bump installer version to v1.0.4

--- installer/AssemblyInfo.cs
+++ installer/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Free Island")]
 [assembly: AssemblyProduct("Free Island")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.3.0")]
-[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyFileVersion("1.0.4.0")]
